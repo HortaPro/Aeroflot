9ebc61a3dba2db341c6c82851de5495ecd615ae7
Horta <nastyadsc@gmail.com>
2023-05-28 17:36:17 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp26/AeroflotComparator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26;
+
+public class AeroflotComparator : IComparer<Aeroflot<string>>, IComparer<Aeroflot<int>>
+{
+    public int Compare(Aeroflot<string>? x, Aeroflot<string>? y)
+    {
+        if (x == null) return 1;
+        if (y == null) return -1;
+        return CompareAircraftType(x.AircraftType, y.AircraftType);
+    }
+
+    public int Compare(Aeroflot<int>? x, Aeroflot<int>? y)
+    {
+        if (x == null) return 1;
+        if (y == null) return -1;
+        return CompareAircraftType(x.AircraftType, y.AircraftType);
+    }
+
+    private int CompareAircraftType(string x, string y)
+    {
+        return string.CompareOrdinal(x, y);
+    }
+}
---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,39 @@
+﻿//вариант 4 Анастасия Миронова
+using ConsoleApp26;
+
+var aeroflots = new Aeroflot<int>[4];
+for (var index = 0; index < aeroflots.Length; index++)
+{
+    Console.Write("Введите номер рейса: ");
+    var flightNumber = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите пункт назначения: ");
+    var destination = Console.ReadLine()!;
+
+    Console.Write("Введите тип самолета: ");
+    var aircraftType = Console.ReadLine()!;
+    Console.WriteLine();
+
+    aeroflots[index] = new Aeroflot<int>(flightNumber, destination, aircraftType);
+}
+
+var aeroflotsList = aeroflots.ToList();
+aeroflotsList.Sort((x, y) => y.CompareTo(x));
+
+Console.WriteLine("\nСписок введенных объектов:");
+foreach (var aeroflot in aeroflotsList)
+    aeroflot.Display();
+
+Console.Write("\nВведите интересующий пункт назначения: ");
+var targetDestination = Console.ReadLine()!;
+
+var searchResult = aeroflotsList.FindAll((item) => item.Destination == targetDestination);
+if (searchResult.Count != 0)
+{
+    foreach (var aeroflot in searchResult)
+        aeroflot.Display();
+}
+else
+{
+    Console.WriteLine("Самолеты, идущие в интересующий пункт назначения, отсутствуют");
+}
